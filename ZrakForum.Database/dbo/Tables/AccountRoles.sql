0b73f0157ce9cb499171012588e374d010b6919a
Данијел Митровић <rodjenihm@outlook.com>
2020-02-27 13:45:12 +0100

    Add AccountRoles table

---
+++ ZrakForum.Database/dbo/Tables/AccountRoles.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[AccountRoles]
+(
+	[AccountId]  INT NOT NULL,
+	[RoleId]     INT NOT NULL, 
+    CONSTRAINT [FK_AccountRoles_Accounts] FOREIGN KEY ([AccountId]) REFERENCES [Accounts]([Id]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_AccountRoles_Roles] FOREIGN KEY ([RoleId]) REFERENCES [Roles]([Id]) ON DELETE CASCADE
+)
+
+GO
+CREATE NONCLUSTERED INDEX [IX_AccountRoles_RoleId]
+    ON [dbo].[AccountRoles]([RoleId] ASC);
