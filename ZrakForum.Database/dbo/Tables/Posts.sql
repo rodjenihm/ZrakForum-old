bec6e4ae216cb46184614b95c7ec591d5e6ceff1
Данијел Митровић <rodjenihm@outlook.com>
2020-02-27 15:38:38 +0100

    Add Posts table

---
+++ ZrakForum.Database/dbo/Tables/Posts.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Posts]
+(
+	[Id]         INT NOT NULL,
+	[CreatedAt]  DATETIME2(7)  NOT NULL,
+	[Content]	 NVARCHAR(MAX) NOT NULL,
+	[AuthorId]   INT NOT NULL DEFAULT 1,
+	[ThreadId]   INT NOT NULL,
+	CONSTRAINT [PK_Posts] PRIMARY KEY ([Id] ASC),
+	CONSTRAINT [FK_Posts_Accounts] FOREIGN KEY ([AuthorId]) REFERENCES [Accounts](Id) ON DELETE SET DEFAULT,
+	CONSTRAINT [FK_Posts_Threads] FOREIGN KEY ([ThreadId]) REFERENCES [Threads](Id) ON DELETE CASCADE
+)
--- ZrakForum.Database/dbo/Tables/Threads.sql
+++ ZrakForum.Database/dbo/Tables/Threads.sql
@@ -3,9 +3,9 @@
 	[Id]			INT NOT NULL,
 	[CreatedAt]		DATETIME2(7)  NOT NULL,
 	[Name]			NVARCHAR(255) NOT NULL,
-	[OwnerId]		INT  NOT NULL DEFAULT 1,
+	[AuthorId]		INT  NOT NULL DEFAULT 1,
 	[ForumId]		INT NOT NULL,
     CONSTRAINT [PK_Threads] PRIMARY KEY ([Id] ASC),
-	CONSTRAINT [FK_Threads_Accounts] FOREIGN KEY ([OwnerId]) REFERENCES [Accounts]([Id]) ON DELETE SET DEFAULT,
+	CONSTRAINT [FK_Threads_Accounts] FOREIGN KEY ([AuthorId]) REFERENCES [Accounts]([Id]) ON DELETE SET DEFAULT,
 	CONSTRAINT [FK_Threads_Forums] FOREIGN KEY ([ForumId]) REFERENCES [Forums]([Id]) ON DELETE CASCADE
 )
