77614ea5ea7a0ad1e90a27f6fd348fd3282925c2
Данијел Митровић <rodjenihm@outlook.com>
2020-02-27 13:41:46 +0100

    Add Accounts table

---
+++ ZrakForum.Database/dbo/Tables/Accounts.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Accounts]
+(
+	[Id]			INT NOT NULL,
+	[CreatedAt]		DATETIME2(7)  NOT NULL,
+	[Email]			NVARCHAR(255) NOT NULL,
+	[Username]		NVARCHAR(255) NOT NULL,
+	[PasswordHash]	NVARCHAR(MAX) NOT NULL,
+    CONSTRAINT [PK_Accounts] PRIMARY KEY ([Id] ASC)
+)
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Accounts_Username]
+	ON [dbo].[Accounts] ([Username] ASC)
