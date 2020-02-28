fb68dfb23931ce38e5cd5455e48a055b4587da7d
Данијел Митровић <rodjenihm@outlook.com>
2020-02-28 17:59:08 +0100

    Add BaseEntity

---
+++ ZrakForum.DataAccess/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ZrakForum.DataAccess.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
