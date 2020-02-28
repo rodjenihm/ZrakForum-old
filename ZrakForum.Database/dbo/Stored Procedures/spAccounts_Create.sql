5dbc4d09610ae91334b5b1110dc28ca46f2a626e
Данијел Митровић <rodjenihm@outlook.com>
2020-02-28 09:09:59 +0100

    Add spAccounts_Create stored procedure

---
+++ ZrakForum.Database/dbo/Stored Procedures/spAccounts_Create.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spAccounts_Create]
+	@Email			NVARCHAR(255),
+	@Username		NVARCHAR(30),
+	@PasswordHash   NVARCHAR(48)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO	[dbo].[Accounts]
+	([Email], [Username], [PasswordHash])
+	VALUES
+	(@Email, @Username, @PasswordHash);
+END
