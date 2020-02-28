b287795cfcaa3a8342fff8d34304b611c8a51754
Данијел Митровић <rodjenihm@outlook.com>
2020-02-28 19:13:18 +0100

    Add client library bootstrap

--- ZrakForum.Web/App_Start/UnityConfig.cs
+++ ZrakForum.Web/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using System.Web.Mvc;
 using Unity;
 using Unity.Mvc5;
+using ZrakForum.DataAccess.Repositories;
 
 namespace ZrakForum.Web
 {
@@ -8,13 +9,13 @@ namespace ZrakForum.Web
     {
         public static void RegisterComponents()
         {
-			var container = new UnityContainer();
-            
+            var container = new UnityContainer();
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
+            container.RegisterType<IAccountRepository, DapperAccountRepository>();
             // e.g. container.RegisterType<ITestService, TestService>();
-            
+
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
