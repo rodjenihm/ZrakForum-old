c28b5abd5a74ee052e7f3bb9f5232a4a7254b647
Данијел Митровић <rodjenihm@outlook.com>
2020-02-28 19:20:42 +0100

    Add Account dtos

---
+++ ZrakForum.Web/Models/AccountLoginDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ZrakForum.Web.Models
+{
+    public class AccountLoginDto
+    {
+        [Required(ErrorMessage = "Email adresa je obavezna")]
+        [EmailAddress(ErrorMessage = "Email adresa nije validna")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Šifra je obavezna")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Šifra")]
+        public string Password { get; set; }
+    }
+}
---
+++ ZrakForum.Web/Models/AccountRegisterDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ZrakForum.Web.Models
+{
+    public class AccountRegisterDto
+    {
+        [Required(ErrorMessage = "Email adresa je obavezna")]
+        [EmailAddress(ErrorMessage = "Email adresa nije validna")]
+        [Display(Name = "Email adresa")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Korisničko ime je obavezno")]
+        [StringLength(30, ErrorMessage = "Korisničko ime mora imati najmanje {2} a najviše {1} karaktera", MinimumLength = 4)]
+        [Display(Name = "Korisničko ime")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Šifra je obavezna")]
+        [StringLength(30, ErrorMessage = "Šifra mora imati najmanje {2} a najviše {1} karaktera", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Šifra")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potvrdite šifru")]
+        [Compare("Password", ErrorMessage = "Šifre se ne podudaraju")]
+        public string ConfirmPassword { get; set; }
+    }
+}
