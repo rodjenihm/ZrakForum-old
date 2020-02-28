148b102de33d667364f1cdd96b13a0041739e997
Данијел Митровић <rodjenihm@outlook.com>
2020-02-28 09:50:43 +0100

    Edit AccountRoles table

--- ZrakForum.Database/dbo/Tables/AccountRoles.sql
+++ ZrakForum.Database/dbo/Tables/AccountRoles.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[AccountRoles]
 (
-	[AccountId]  INT NOT NULL,
-	[RoleId]     INT NOT NULL, 
+	[AccountId]     INT NOT NULL,
+	[RoleId]        INT NOT NULL, 
+    [AssignedAt]    DATETIME2(7) NOT NULL DEFAULT GETDATE(),
     CONSTRAINT [PK_AccountRoles] PRIMARY KEY CLUSTERED ([AccountId] ASC, [RoleId] ASC),
     CONSTRAINT [FK_AccountRoles_Accounts] FOREIGN KEY ([AccountId]) REFERENCES [Accounts]([Id]) ON DELETE CASCADE, 
     CONSTRAINT [FK_AccountRoles_Roles] FOREIGN KEY ([RoleId]) REFERENCES [Roles]([Id]) ON DELETE CASCADE
