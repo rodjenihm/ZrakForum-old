7d240f9224e67610c8a9151c672bf454c5f29d50
Данијел Митровић <rodjenihm@outlook.com>
2020-03-01 00:41:44 +0100

    Add ForumRoute and ForumController

--- ZrakForum.Web/App_Start/RouteConfig.cs
+++ ZrakForum.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,13 @@ namespace ZrakForum.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Forum",
+                 url: "Forum/{forumName}",
+                 defaults: new { controller = "Forum", action = "Index" }
+                 //constraints: new { forumName = @"\d+" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
