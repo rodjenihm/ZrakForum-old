49e8d6c10910ae9ca7e3823b05f66ec38cb36741
Данијел Митровић <rodjenihm@outlook.com>
2020-03-03 12:40:11 +0100

    Add stored procedure to load ForumIndex vm

---
+++ ZrakForum.Database/dbo/Stored Procedures/spThreads_GetForumIndexViewModel.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[spThreads_GetForumIndexViewModel]
+	@ForumName NVARCHAR(255)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @ForumId INT;
+
+	SET @ForumId = 
+	(
+		SELECT [Id] FROM [dbo].[Forums]
+		WHERE [Name] = @ForumName
+	);
+
+	SELECT [Threads].[Id] as [ThreadId], [Threads].[CreatedAt], [Threads].[Name], [Accounts].[Username] 
+	FROM [dbo].[Threads]
+	JOIN [dbo].[Accounts]
+	ON ([Threads].[AuthorId] = [Accounts].[Id])
+	WHERE [ForumId] = @ForumId;
+END
